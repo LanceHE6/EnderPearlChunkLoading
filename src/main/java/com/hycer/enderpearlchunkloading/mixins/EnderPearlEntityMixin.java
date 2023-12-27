2eaa59ebdc548aa1126b5c6e400bc04c95b4d9f6
LanceHE6 <2765543491@qq.com>
2023-12-27 19:31:49 +0800

    chaos.更改注入类名，优化继承 feat.新增加载次数限制，超过限制将移除珍珠实体，防止崩服 feat.新增加载信息输出

--- src/main/java/com/hycer/enderpearlchunkloading/mixins/EnderPearlEntityMixin.java
+++ src/main/java/com/hycer/enderpearlchunkloading/mixins/EnderPearlEntityMixin.java
@@ -1,7 +1,9 @@
 package com.hycer.enderpearlchunkloading.mixins;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -10,18 +12,22 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
 import net.minecraft.world.World;
 
+import java.util.Objects;
 
-@Mixin(ThrownEntity.class)
-public abstract class ThrownEntityMixin extends Entity {
+
+@Mixin(EnderPearlEntity.class)
+public abstract class EnderPearlEntityMixin extends ThrownEntity {
     @Unique
-    private static ChunkPos prevChunkPos = null;
-    private ThrownEntityMixin(EntityType<?> type, World world)
+    private ChunkPos prevChunkPos = null;
+    @Unique
+    private int loadCount = 0;
+
+    private EnderPearlEntityMixin(EntityType<? extends ThrownEntity> type, World world)
     {
         super(type, world);
     }
@@ -31,29 +37,34 @@ public abstract class ThrownEntityMixin extends Entity {
     {
         if (((Object) this) instanceof EnderPearlEntity)
         {
+                PlayerEntity owner = (PlayerEntity) this.getOwner();
                 World world = this.getEntityWorld();
                 Vec3d velocity = this.getVelocity();
 
                 // 非珍珠炮射出的珍珠不会触发强加载
                 if (world instanceof ServerWorld &&
                         (Math.abs(velocity.x) > 20 || Math.abs(velocity.z) > 20)) {
+                    if (loadCount > 20){
+//                        System.out.println("超过20gt未被拦截，移除珍珠实体");
+                        owner.sendMessage(Text.of("[EnderPearlChunkLoading]珍珠超过20gt未被拦截，已移除珍珠实体"));
+                        this.kill();
+                    }
                     Vec3d pos = this.getPos();
                     double nx = pos.x + velocity.x;
                     double nz = pos.z + velocity.z;
-                    System.out.println("Adding ticket for " + nx + " " + nz);
 
                     ChunkPos cp = new ChunkPos(MathHelper.floor(nx) >> 4, MathHelper.floor(nz) >> 4);
 
-//                    System.out.println("Adding chunk ticket for " + cp.x + " " + cp.z);
-//                    System.out.println("before,isChunkLoaded: " + ((ServerWorld) world).getChunkManager().isChunkLoaded(cp.x, cp.z));
-
                     ServerChunkManager chunkManager = ((ServerWorld) world).getChunkManager();
                     // 取消之前强加载的区块
-                    if (prevChunkPos != null && prevChunkPos != cp) {
+                    if (prevChunkPos != null && !Objects.equals(prevChunkPos, cp)) {
                         chunkManager.setChunkForced(prevChunkPos, false);
                     }
                     // 设置新的强加载区块
                     chunkManager.setChunkForced(cp, true);
+                    owner.sendMessage(Text.of("[EPCL]已加载区块: " + String.format("%.2f", nx) + " " + String.format("%.2f", nz)));
+                    System.out.println("Set chunk forced for " + nx + " " + nz);
+                    loadCount ++;
                     prevChunkPos = cp;
                     chunkManager.setChunkForced(cp, true);
 //                    System.out.println("after,isChunkLoaded: " + ((ServerWorld) world).getChunkManager().isChunkLoaded(cp.x, cp.z));
